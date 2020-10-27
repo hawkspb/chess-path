915eb017e0c21d8961624f91842f22173b6fc2b3
u_m15ps <PChistyakov@alfabank.ru>
2020-10-27 15:39:45 +0300

    Source compatibility for gradle run

--- src/main/java/com/example/chesspath/controller/ApiControllerAdvice.java
+++ src/main/java/com/example/chesspath/controller/ApiControllerAdvice.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.util.Collections;
 import java.util.Map;
